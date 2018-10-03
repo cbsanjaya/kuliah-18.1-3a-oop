47380c1ee27fa73ae1f89cf204cc2be93deb6d96
Cahya Bagus Sanjaya <9c96b6@gmail.com>
2018-10-03 11:16:52 +0700

    Buat Objek Semut

--- oop/Dinding.java
+++ oop/Dinding.java
@@ -18,18 +18,9 @@ public class Dinding extends World
         // Create a new world with 600x400 cells with a cell size of 1x1 pixels.
         super(600, 400, 1);
         
-        // membuat obyek lalat baru
-        Lalat lalatPutih = new Lalat();
+        Semut semut = new Semut();
         
-        // tambahkan lalat putih ke Dinding
-        addObject(lalatPutih, 50, 100);
-        
-        // Lalat cepat = new Lalat();
-        // cepat.setKecepatan(2);
-        
-        // addObject(cepat, 50, 200);
-        
-        
+        addObject(semut, 300, 200);
         
     }
 
