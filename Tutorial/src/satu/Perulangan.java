1722286941658421f16ba0ea918cee0544aa0dcc
Cahya Bagus Sanjaya <9c96b6@gmail.com>
2018-11-28 12:42:52 +0700

    Perulangan

---
+++ Tutorial/src/satu/Perulangan.java
@@ -0,0 +1,37 @@
+/*
+ * To change this license header, choose License Headers in Project Properties.
+ * To change this template file, choose Tools | Templates
+ * and open the template in the editor.
+ */
+package satu;
+
+/**
+ *
+ * @author cahya
+ */
+public class Perulangan {
+    public static void main(String[] args) {
+        
+//        for (int i = 0; i < 100; i++) {
+//            System.out.println(i);
+//        }
+//        
+//        for (int i = 50; i >= 40; i--) {
+//            if (i % 2 != 0) {
+//                System.out.println(i);
+//            }
+//        }
+//       
+        int i = 0;
+        
+        while (i < 0) {            
+            System.out.println(i);
+            i++;
+        }
+
+//        do {
+//            System.out.println(i);
+//            i++;
+//        } while ( i < 0);
+    }
+}
