7002325c92c2993d2272f5ca476ab9d92a7b9a40
Cahya Bagus Sanjaya <9c96b6@gmail.com>
2018-12-01 13:04:40 +0700

    class and object

---
+++ Tutorial/src/dua/Index.java
@@ -0,0 +1,11 @@
+package dua;
+
+public class Index {
+    public static void main(String[] args) {
+        
+        PersegiPanjang pj = new PersegiPanjang(5);
+        
+        System.out.println(pj.toString());
+        
+    }
+}
---
+++ Tutorial/src/dua/PersegiPanjang.java
@@ -0,0 +1,23 @@
+package dua;
+
+public class PersegiPanjang {
+    
+    private int panjang;
+    private int lebar;
+    
+    public PersegiPanjang() {
+        this.panjang = 5;
+        this.lebar = 5;
+    }
+    
+    public PersegiPanjang(int panjang, int lebar) {
+        this.panjang = panjang;
+        this.lebar = lebar;
+    }
+    
+    public PersegiPanjang(int panjang) {
+        this.panjang = panjang;
+        this.lebar = 5;
+    }
+    
+}
